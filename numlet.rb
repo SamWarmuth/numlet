70ea8ab72359b0df03178ae662db621bef4181ac
Sam Warmuth <sam@samwarmuth.com>
2010-06-15 20:24:41 -0500

    refactored to_words for clarity

--- numlet.rb
+++ numlet.rb
@@ -1,18 +1,22 @@
 class Integer
   Language = {
-    "en" => {:ones  => %w[zero one two three four five six seven eight nine ten 
-                          eleven twelve thirteen fourteen fifteen sixteen seventeen eighteen nineteen], 
-             :tens  => %w[zero ten twenty thirty forty fifty sixty seventy eighty ninety], 
+    "en" => {:ones  => %w[one two three four five six seven eight nine ten 
+                          eleven twelve thirteen fourteen fifteen sixteen seventeen eighteen nineteen].unshift(""), 
+             :tens  => %w[ten twenty thirty forty fifty sixty seventy eighty ninety].unshift(""), 
              :hundred => "hundred",
-             :large => %w[thousand million billion trillion].insert(0, ""),
-             :tens_connect => "-"},
+             :large => %w[thousand million billion trillion].unshift(""),
+             :tens_connect => "-",
+             :zero => "zero"},
              
-    "es" => {:ones  => %w[cero uno dos tres cuatro cinco seis siete ocho nueve diez
-                          once doce trece catorce quince dieciséis diecisiete dieciocho diecinueve],
-             :tens  => %w[cero diez veinte treinta cuarenta cincuenta sesenta setenta ochenta noventa],
+    "es" => {:ones  => %w[uno dos tres cuatro cinco seis siete ocho nueve diez
+                          once doce trece catorce quince dieciséis diecisiete dieciocho diecinueve
+                          veinte veintiuno veintidós veintitrés veinticuatro veinticinco
+                          veintiséis veintisiete veintiocho veintinueve].unshift(""),
+             :tens  => %w[diez veinte treinta cuarenta cincuenta sesenta setenta ochenta noventa].unshift(""),
              :hundred => "cien",
-             :large => %w[mil millón billion trillion].insert(0, ""),
-             :tens_connect => " y "}
+             :large => %w[mil millón billion trillion].unshift(""),
+             :tens_connect => " y ",
+             :zero => "cero"}
     }
 
    def to_words(args = {})
@@ -23,11 +27,24 @@ class Integer
      triplets = to_s.reverse.scan(/\d{1,3}/).map{|l| l.reverse.to_i}
      output = ""
      triplets.each_with_index do |triplet, index|
-       tens = (triplet%100 < 20 ? language[:ones][triplet%100] : (language[:tens][triplet%100/10] + (triplet%10 == 0 ? "" : language[:tens_connect] + language[:ones][triplet%10])))
-       hundreds = (triplet/100 == 0 ? "" : language[:ones][triplet/100] + " " + language[:hundred])
+       next if triplet == 0
+       
+       if triplet%100 < language[:ones].length
+         tens = language[:ones][triplet%100]
+       else
+         tens = language[:tens][triplet%100/10] + (triplet%10 == 0 ? "" : language[:tens_connect] + language[:ones][triplet%10])
+       end
+       
+       if triplet/100 == 0
+         hundreds = ""
+       else
+         hundreds = language[:ones][triplet/100] + " " + language[:hundred]
+       end
+
        large = language[:large][index]
        output = hundreds + " " + tens + " "+large+ " " + output
      end
+     output = language[:zero] if output.strip.empty?
      return output.strip
    end
 end
--- test.rb
+++ test.rb
@@ -4,21 +4,30 @@ class BasicTests < Test::Unit::TestCase
   def test_number_is_number
     assert_equal(55.class, Fixnum)
   end
-  
-  def test_sub_hundred_no_args
-    assert_equal(55.to_words, "fifty-five")
+end
+
+class EnglishTest < Test::Unit::TestCase
+  def setup
+    @numbers = File.read('cardinals/en.txt').map{|l| l = l.split(","); [l[0].to_i, l[1].strip]}
   end
   
-  def test_over_hundred_no_args
-    assert_equal(392.to_words, "three hundred ninety-two")
+  def test_number_to_words
+    @numbers.each do |pair|
+      assert_equal(pair[1], pair[0].to_words(:lang => 'en'))
+    end
   end
   
-  def test_sub_hundred_en
-    assert_equal("fifty-five", 55.to_words(:lang => 'en'))
+end
+
+class SpanishTest < Test::Unit::TestCase
+  def setup
+    @numbers = File.read('cardinals/es.txt').map{|l| l = l.split(","); [l[0].to_i, l[1].strip]}
   end
   
-  def test_over_hundred_en
-    assert_equal("three hundred ninety-two", 392.to_words(:lang => 'en'))
+  def test_number_is_number
+    @numbers.each do |pair|
+      assert_equal(pair[1], pair[0].to_words(:lang => 'es'))
+    end
   end
   
 end
